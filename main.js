ac8ba74cae8099bbc7c710ffcfc74a14b6a038fa
Jeff Gore <jgore00@gmail.com>
2018-10-13 16:25:29 -0400

    remove boilerplate database and add deploy script

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 'use strict';
 
-const db = require('./server/db/models');
 const app = require('./server');
-const PORT = 1337;
+const PORT = process.env.PORT || 1337;
 
-db.sync() // if you update your db schemas, make sure you drop the tables first and then recreate them
-  .then(() => {
-    console.log('Database synced.');
-    app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
-  });
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
