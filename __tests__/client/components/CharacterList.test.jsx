d730ea5815619233dd2f7ab19638544c8e036ac6
Jeff Gore <jgore00@gmail.com>
2019-04-04 08:37:32 -0400

    full coverage on CharacterList component

--- __tests__/client/components/CharacterList.test.jsx
+++ __tests__/client/components/CharacterList.test.jsx
@@ -3,54 +3,63 @@ import { shallow, mount } from 'enzyme';
 import CharacterList from '../../../src/client/components/CharacterList';
 import CharacterCard from '../../../src/client/components/CharacterCard';
 import Loading from '../../../src/client/components/Loading';
-
-const onClickSpy = jest.fn();
+import ErrorMessage from '../../../src/client/components/ErrorMessage';
 
 describe('CharacterList ', () => {
   it('should render a <Loading> spinner if selectedCharacter is true and no error exists', () => {
     const list = shallow(
-      <CharacterList characters={['Obi', 'Luke']} selectedCharacter="Obi"/>
+      <CharacterList
+        characters={[{ id: 1, name: 'Obi Wan Kenobi' }]}
+        selectedCharacter={{ id: 1, name: 'Obi Wan Kenobi' }}
+      />
     );
-    console.log(list.debug())
     expect(list.exists(Loading)).toBe(true);
   });
-});
 
-/* import React from 'react';
-import CharacterCard from './CharacterCard';
-import ErrorMessage from './ErrorMessage';
-import Loading from './Loading';
+  it('should not render a loader if selectedCharacter is false and no error exists', () => {
+    const list = shallow(<CharacterList characters={['Obi', 'Luke']} />);
+    expect(list.exists(Loading)).toBe(false);
+  });
 
-const CharacterList = ({
-  characters,
-  handleCharacterSelect,
-  handleCharacterDeselect,
-  selectedCharacter,
-  error
-}) => (
-  <div id="character-list">
-    {selectedCharacter && !error ? <Loading /> : null}
-    {characters
-      ? characters
-          .sort((a, b) => a.order - b.order)
-          .map(character => (
-            <CharacterCard
-              key={character.id}
-              name={character.name}
-              onClick={event => handleCharacterSelect(event, character.id)}
-            />
-          ))
-      : null}
-    {selectedCharacter && selectedCharacter.id && error ? (
-      <ErrorMessage
-        error={error}
-        execRollbackActions={() =>
-          handleCharacterDeselect(null, selectedCharacter.id)
-        }
+  it('should not render CharacterCard components if characters prop is falsy', () => {
+    const list = shallow(<CharacterList />);
+    expect(list.exists(CharacterCard)).toBe(false);
+  });
+
+  it('should render an ErrorMessage if a character has been selected and there is an error', () => {
+    const list = shallow(
+      <CharacterList
+        selectedCharacter={{ id: 1, name: 'Obi Wan Kenobi' }}
+        error={new Error('load failure')}
       />
-    ) : null}
-  </div>
-);
+    );
+    expect(list.exists(ErrorMessage)).toBe(true);
+  });
 
-export default CharacterList;
- */
+  it('should pass the CharacterCard an onClick handler which, when the card is clicked, calls the handleCharacterSelect handler', () => {
+    const handleCharacterSelect = jest.fn();
+    const list = shallow(
+      <CharacterList
+        handleCharacterSelect={handleCharacterSelect}
+        characters={[{ id: 1, name: 'Obi Wan Kenobi' }]}
+      />
+    );
+    const card = list.find(CharacterCard).first();
+    card.props().onClick();
+    expect(handleCharacterSelect).toHaveBeenCalled();
+  });
+
+  it('should pass the ErrorMessage an execRollbackActions handler which, when the card is clicked, calls the handleCharacterDeselect handler', () => {
+    const handleCharacterDeselect = jest.fn();
+    const list = shallow(
+      <CharacterList
+        selectedCharacter={{ id: 1, name: 'Obi Wan Kenobi' }}
+        error={new Error('load failure')}
+        handleCharacterDeselect={handleCharacterDeselect}
+      />
+    );
+    const errorMessage = list.find(ErrorMessage).first();
+    errorMessage.props().execRollbackActions();
+    expect(handleCharacterDeselect).toHaveBeenCalled();
+  });
+});
