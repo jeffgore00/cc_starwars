e06ed89d189918826b1d9cc88a4d555653791dc6
Jeff Gore <jgore00@gmail.com>
2019-04-07 15:03:06 -0400

    update dependencies

---
+++ __tests__/server/api/index.test.js
@@ -0,0 +1,25 @@
+// import request from 'supertest';
+// import app from '../../src/server';
+// import utils from '../../src/server/utils-server';
+
+describe('API routes', () => {
+  it('retrieves characters', () => {
+
+  });
+
+  it('retrieves films', () => {
+
+  });
+
+  it('handles errors retrieving character details - SWAPI', () => {
+
+  });
+
+  it('handles errors retrieving character films (all) - SWAPI', () => {
+
+  });
+
+  it('handles errors retrieving character films (some) - SWAPI', () => {
+
+  });
+});
