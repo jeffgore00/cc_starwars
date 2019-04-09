c89cafd5bdf24a26b3eed1f834c3af2c36010b17
Jeff Gore <jgore00@gmail.com>
2019-04-08 22:53:52 -0400

    more tweaks

--- src/client/utils-client.js
+++ src/client/utils-client.js
@@ -9,25 +9,23 @@ export function buildErrorMessage(source, statusCode) {
   if (statusCode === 404) {
     if (source === 'SWAPI') {
       return 'The requested content could not be found on the SWAPI server.';
-    } else {
-      return 'The requested content could not be found.';
     }
-  } else if (statusCode === 500) {
+    return 'The requested content could not be found.';
+  }
+  if (statusCode === 500) {
     if (source === 'SWAPI') {
       return 'There was a SWAPI internal server error. Please try your request again later.';
-    } else {
-      return 'There was an internal server error. It was been logged and we will investigate. Our apologies!';
     }
-  } else {
-    return 'There was an error. Please try again later.';
+    return 'There was an internal server error. It was been logged and we will investigate. Our apologies!';
   }
+  return 'There was an error. Please try again later.';
 }
 
 // Code source: https://stackoverflow.com/questions/9083037/
 export function romanize(num) {
   if (isNaN(num)) return NaN;
-  var digits = String(+num).split(''),
-    key = [
+  const digits = String(+num).split('');
+    const key = [
       '',
       'C',
       'CC',
@@ -58,9 +56,9 @@ export function romanize(num) {
       'VII',
       'VIII',
       'IX'
-    ],
-    roman = '',
-    i = 3;
+    ];
+    let roman = '';
+    let i = 3;
   while (i--) roman = (key[+digits.pop() + i * 10] || '') + roman;
-  return Array(+digits.join('') + 1).join('M') + roman;
+  return new Array(+digits.join('') + 1).join('M') + roman;
 }
