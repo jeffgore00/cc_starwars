1ff7ce52e77075bcf1654d174829d8b30e5cc951
Jeff Gore <jeff.gore@aexp.com>
2019-04-11 11:42:06 -0400

    add fix #3

--- fixes.js
+++ fixes.js
@@ -1,4 +1,5 @@
 // --------0. Doesn’t display the font correctly.
+// ADD: 
 it('should render with the correct styling', () => {
   const component = mount(<CharacterListPage />);
   const element = component.getDOMNode();
@@ -6,6 +7,7 @@ it('should render with the correct styling', () => {
 });
 
 // --------1. Doesn’t display the characters
+// ADD:
 it('should pass along the character data it receives to the character list', () => {
   const container = mount(<CharacterListPage characters={['Obi', 'Luke']} {...requiredProps} />);
   const list = container.find(CharacterList)
@@ -13,6 +15,11 @@ it('should pass along the character data it receives to the character list', () 
 })
 
 // --------2. Doesn’t respond to an onClick (I. No cursor)
-
+// REPLACE in <CharacterCard>
+it('should have a click handler that handles character selection when called', () => {
+  const card = shallow(<CharacterCard key="1" name="Emperor Palpatine" onClick={onClickMock} />);
+  card.simulate('click')
+  expect(handleCharacterSelect).toHaveBeenCalled();
+});
 
 // --------3. Doesn’t respond to an onClick (II. No good response)
