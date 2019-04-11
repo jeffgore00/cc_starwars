80f6015fd7bc526e5f9200828d61675efa308d5a
Jeff Gore <jeff.gore@aexp.com>
2019-04-11 11:27:29 -0400

    update fixes

--- fixes.js
+++ fixes.js
@@ -1,14 +1,16 @@
 // --------0. Doesn’t display the font correctly.
 it('should render with the correct styling', () => {
-    const component = mount(<CharacterListPage />);
-    const element = component.getDOMNode()
-    expect(element.getAttribute('id')).toEqual('entire-page-wrapper')
-  })
-
+  const component = mount(<CharacterListPage />);
+  const element = component.getDOMNode();
+  expect(element.getAttribute('id')).toEqual('entire-page-wrapper');
+});
 
 // --------1. Doesn’t display the characters
-
-
+it('should pass along the character data it receives to the character list', () => {
+  const container = mount(<CharacterListPage characters={['Obi', 'Luke']} {...requiredProps} />);
+  const list = container.find(CharacterList)
+  expect(container.props().characters).toBe(list.props().characters)
+})
 
 // --------2. Doesn’t respond to an onClick (I. No cursor)
 
